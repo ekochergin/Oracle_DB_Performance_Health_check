4a5ffc7f919633f41d4e1fc79bc2cfd530e897f0
ekochergin <66642134+ekochergin@users.noreply.github.com>
2020-06-24 22:31:49 +0200

    Add stale statistics
    Added info about tables and indexes having stale statistics.
    
    JS-procedure "show_command" has been introduced

--- HTML_DB_Perf_Report.sql
+++ HTML_DB_Perf_Report.sql
@@ -51,7 +51,7 @@ declare
   end get_binds;
   
   /*
-  procedure is to print a clob
+  prints a clob to the output a clob
   */
   procedure print_clob(p_clob in clob)
   is
@@ -97,7 +97,40 @@ declare
   end escape_html_clob;
   
   /*
-  puts out sqls' ferormance statistic in an html table
+  prints a simple html-table, puts a "good-news" div when nothing found
+  parameters: p_id - the id html tag for the table
+              p_headers - a bunch of th tags to be printed within thead tag
+              p_cursor - sys_refcursor to get data. It must return just a varchar-row containing valid html table row (<tr>...</tr>)
+              p_classes - list of css-classes associated with the table (varchar2, default is null)
+  */
+  procedure simple_html_table(p_id in varchar2, p_headers in varchar2, p_cursor in sys_refcursor, p_classes in varchar2 default null)
+  is
+    type trs is table of varchar2(32000);
+    tr_lines trs;
+  begin
+    fetch p_cursor bulk collect into tr_lines;
+    close p_cursor;
+    
+    if tr_lines.count > 0 then
+      dbms_output.put_line('<table id="' || p_id || '" class="' || p_classes || '">');
+      dbms_output.put_line('<thead>' || p_headers || '</thead>');
+      dbms_output.put_line('<tbody>');
+      for i in 1..tr_lines.count loop
+        dbms_output.put_line(tr_lines(i));
+      end loop;
+      dbms_output.put_line('</tbody></table>');
+    else
+      dbms_output.put_line('<div id="good-news">Nothing found!</div>');
+    end if;
+  exception
+    when others then
+      if p_cursor%isopen then
+        close p_cursor;
+      end if;
+  end simple_html_table;
+  
+  /*
+  puts out sqls' performance statistic in an html table
   parameters: p_stat_name - statistic name sqls are to be sorted by 
                 options:  'time' - get queries sorted by elapsed_time descending
                           'disk' - sorted by disk_reads desc
@@ -109,17 +142,16 @@ declare
   is
     -- cursor to show sqls' performance statistics. 
     cursor stats_data(p_order_by in varchar2) is
-      select '<tr>' ||
-             '<td class="left-align">' || s.sql_id || '</td>' ||
-             '<td class="left-align">' || s.child_address || '</td>' ||
-             '<td>' || s.END_OF_FETCH_COUNT || '</td>' ||
-             '<td>' || round(s.elapsed_time / s.END_OF_FETCH_COUNT) * 1000000 || '</td>' ||
-             '<td>' || round(s.disk_reads / s.END_OF_FETCH_COUNT) || '</td>' || 
-             '<td>' || round(s.buffer_gets / s.END_OF_FETCH_COUNT) || '</td>' ||
-             '<td>' || round(s.cpu_time / s.END_OF_FETCH_COUNT) || '</td>' ||
-             '<td class="left-align">' || s.module || '</td>' ||
-             '<td class="center-align"><a href="#" onclick="gather_info(''' || s.sql_id || ''', ''' || s.child_address || ''')">Show info</a></td>' ||
-             '</tr>' as val,
+      select '<tr><td class="left-align">' || s.sql_id ||
+             '</td><td class="left-align">' || s.child_address ||
+             '</td><td>' || s.END_OF_FETCH_COUNT ||
+             '</td><td>' || round(s.elapsed_time / s.END_OF_FETCH_COUNT) * 1000000 ||
+             '</td><td>' || round(s.disk_reads / s.END_OF_FETCH_COUNT) ||
+             '</td><td>' || round(s.buffer_gets / s.END_OF_FETCH_COUNT) ||
+             '</td><td>' || round(s.cpu_time / s.END_OF_FETCH_COUNT) ||
+             '</td><td class="left-align">' || s.module ||
+             '</td><td class="center-align"><a href="#" onclick="gather_info(''' || s.sql_id || ''', ''' || s.child_address || ''')">Show query info</a>' ||
+             '</td></tr>' as val,
              s.sql_fulltext,
              s.sql_id,
              s.child_address
@@ -170,49 +202,80 @@ declare
                            '<p>  child_address: ' || l_child_address ||';</p>');
       dbms_output.put_line('</div');
   end print_perf_stats_data;
-
+  
   procedure print_invalid_objects
   is
-    cursor c_inv_objs is
-      select '<tr><td class="left-align">' || owner || 
-             '</td><td class="left-align">' || object_name || 
-             '</td><td class="left-align">' || object_type || 
-             '</td><td class="right-align">' || to_char(last_ddl_time, 'dd.mm.yyyy hh24:mi:ss') ||
-             '</td><td class="left-align">' || case object_type 
-                                                 when 'PACKAGE BODY' then 'alter package '  || owner ||'.' || object_name || ' compile body;'
-                                                 else 'alter ' || object_type || ' ' || owner ||'.' || object_name || ' compile;'
-                                               end || 
-             '</td></tr>' as tr
-         from dba_objects
-                     where status = 'INVALID';
-                     
-    type inv_objs_t is table of c_inv_objs%rowtype;
-    inv_objs inv_objs_t;
+    c_inv_objs sys_refcursor;
   begin
-    open c_inv_objs;
-    fetch c_inv_objs bulk collect into inv_objs;
-    close c_inv_objs;
-    
-    if inv_objs.count > 1 then
-      dbms_output.put_line('<table>');
-      dbms_output.put_line('<thead><th>owner</th><th>name</th><th>type</th><th>last ddl time</th><th>How to fix</th></thead>');
-      for i in 1..inv_objs.count loop
-        dbms_output.put_line(inv_objs(i).tr);
-      end loop;
-      dbms_output.put_line('</table>');
-    else
-      dbms_output.put_line('<div id="good-news">No invalid objects found!</div>');
-    end if;
+    open c_inv_objs for select '<tr><td class="left-align">'   || owner || 
+                               '</td><td class="left-align">'  || object_name || 
+                               '</td><td class="left-align">'  || object_type || 
+                               '</td><td class="right-align">' || to_char(last_ddl_time, 'dd.mm.yyyy hh24:mi:ss') ||
+                               '</td><td class="left-align">'  || case object_type 
+                                                                    when 'PACKAGE BODY' then 'alter package '  || owner ||'.' || object_name || ' compile body;'
+                                                                    else 'alter ' || lower(object_type) || ' ' || owner ||'.' || object_name || ' compile;'
+                                                                  end || 
+                               '</td></tr>' as tr
+                          from dba_objects
+                         where status = 'INVALID';
+     simple_html_table('invObjs', 
+                       '<th>owner</th><th>name</th><th>type</th><th>last ddl time</th><th>How to fix</th>',
+                       c_inv_objs);
+  exception
+    when others then 
+      dbms_output.put_line('<div class="bad-news">');
+      dbms_output.put_line('There is a following error in printing invalid objects procedure: ' || sqlerrm);
+      dbms_output.put_line('</div');   
+  end print_invalid_objects;
+  
+  procedure print_stale_tables
+  is 
+    c_stale_tabs sys_refcursor;
+  begin
+    open c_stale_tabs for select '<tr><td class="left-align">'  || owner ||
+                                 '</td><td class="left-align">' || table_name ||
+                                 '</td><td class="left-align">' || object_type ||
+                                 '</td><td class="right-align">' || to_char(last_analyzed, 'dd.mm.yyyy hh24:mi:ss') ||
+                                 '</td><td class="left-align">' || 'begin dbms_stats.gather_table_stats(''' || owner || ''', ''' || table_name || '''' || 
+                                 case when partition_name is not null then ', ''' || partition_name || '''' end || '); end;' ||
+                                 '</td></tr>'
+                            from dba_tab_statistics 
+                           where stale_stats = 'YES';
+    simple_html_table('staleTables', 
+                      '<th> owner </th><th> table name </th><th> object type </th><th> last analyzed date </th><th> how to fix </th>', 
+                      c_stale_tabs);
+  exception
+    when others then 
+      dbms_output.put_line('<div class="bad-news">');
+      dbms_output.put_line('There is a following in printing stale tables procedure: ' || sqlerrm);
+      dbms_output.put_line('</div');  
+  end print_stale_tables;
+
+  procedure print_stale_indexes
+  is
+    c_stale_indxs sys_refcursor;
+  begin
+    open c_stale_indxs for select '<tr><td class="left-align">'  || owner ||
+                                  '</td><td class="left-align">' || index_name ||
+                                  '</td><td class="left-align">' || table_owner || 
+                                  '</td><td class="left-align">' || table_name ||
+                                  '</td><td class="left-align">' || object_type ||
+                                  '</td><td class="right-align">' || to_char(last_analyzed, 'dd.mm.yyyy hh24:mi:ss') ||
+                                  '</td><td class="center-align">' || 
+                                    '<a href=# onclick="show_command(''begin dbms_stats.gather_index_stats(\''' || owner || '\'', \''' || index_name || '\''' || 
+                                    case when partition_name is not null then ', \''' || partition_name || '\''' end || '); end;'')"' || '>Show command</a>' ||
+                                  '</td></tr>'     
+                             from dba_ind_statistics 
+                            where stale_stats = 'YES';
+    simple_html_table('staleIndexes',
+                      '<th> owner </th><th> index name </th><th> table owner </th><th> table name </th><th> object type </th><th> last analyzed date </th><th> how to fix </th>',
+                      c_stale_indxs);
   exception
     when others then
-      if c_inv_objs%isopen then
-        close c_inv_objs;
-      end if;  
-    
       dbms_output.put_line('<div class="bad-news">');
-      dbms_output.put_line('Error in print_invalid_objects: ' || sqlerrm);
-      dbms_output.put_line('</div');     
-  end print_invalid_objects;
+      dbms_output.put_line('There is a following error in the printing stale indexes procedure: ' || sqlerrm);
+      dbms_output.put_line('</div');        
+  end print_stale_indexes;
 
 begin
   -- CSS starts
@@ -297,6 +360,13 @@ begin
 
   dbms_output.put_line('<body>');  
     dbms_output.put_line('<div id="popup-background" class="popup-back hidden"></div>'); -- div to be shown as popup's background
+    -- objects having staled statistics
+    dbms_output.put_line('<h2> Objects having stale statistics </h2>');
+    dbms_output.put_line('<h3> Tables </h3>');
+    print_stale_tables();
+
+    dbms_output.put_line('<h3> Indexes </h3>');
+    print_stale_indexes();
 
     -- show invalid objects
     dbms_output.put_line('<h3> Invalid objects </h3>');
